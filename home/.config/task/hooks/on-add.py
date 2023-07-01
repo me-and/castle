d1017bbc71aa942d0eec8f455208dfd99f384b55
Adam Dinwoodie <adam@dinwoodie.org>
2023-07-01 17:43:07 +0100

    fixup! Taskwarrior: move to library

--- home/.config/task/hooks/on-add.py
+++ home/.config/task/hooks/on-add.py
@@ -3,4 +3,4 @@
 from asmodeus.hook import on_add, inbox, child_until, recur_after, due_end_of
 
 if __name__ == '__main__':
-    on_add(due_end_of, recur_after, child_until, inbox)
+    on_add((due_end_of, recur_after, child_until, inbox))
