c70748c9c1ef1434611dbb8f88bbe2f9d14c3777
Adam Dinwoodie <adam@dinwoodie.org>
2023-07-02 16:55:24 +0100

    TaskWarrior: fix hooks

--- home/.config/task/hooks/on-add.py
+++ home/.config/task/hooks/on-add.py
@@ -4,4 +4,4 @@ from asmodeus.hook import on_add, inbox, child_until, recur_after, due_end_of
 from asmodeus.taskwarrior import TaskWarrior
 
 if __name__ == '__main__':
-    on_add(TaskWarrior(), ())
+    on_add(TaskWarrior(), (inbox, child_until, recur_after, due_end_of))
--- home/.config/task/hooks/on-modify.py
+++ home/.config/task/hooks/on-modify.py
@@ -4,4 +4,4 @@ from asmodeus.hook import on_modify, child_until, recur_after, due_end_of
 from asmodeus.taskwarrior import TaskWarrior
 
 if __name__ == '__main__':
-    on_modify(TaskWarrior(), ())
+    on_modify(TaskWarrior(), (child_until, recur_after, due_end_of))
