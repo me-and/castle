9bc1de3f12353019c2d24f1f4f284ba7631cf7fa
Adam Dinwoodie <adam@dinwoodie.org>
2023-07-06 09:06:12 +0100

    TaskWarrior: separate work/non-work hooks

--- home/.config/task/hooks/on-add.py
+++ home/.config/task/hooks/on-add.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
+import subprocess
 
 import asmodeus.hook as h
 from asmodeus.taskwarrior import TaskWarrior
 
 if __name__ == '__main__':
-    h.on_add(TaskWarrior(), (h.inbox, h.child_until, h.recur_after,
-                             h.due_end_of, h.reviewed_to_entry))
+    tw = TaskWarrior()
+    hooks: list[h.OnAddHook] = [h.child_until,
+                                h.recur_after,
+                                h.due_end_of,
+                                h.reviewed_to_entry,
+                                ]
+    try:
+        # Work systems have a tickets report set up.
+        tw.get_dom('rc.report.tickets.filter')
+    except subprocess.CalledProcessError:
+        # Not work, meaning I want to tag anything as "inbox" if it doesn't
+        # already have a tag.
+        hooks.append(h.inbox)
+    h.on_add(tw, hooks)
