efebae78dda029e02a4fad36d21bebef1efb3633
Adam Dinwoodie <adam@dinwoodie.org>
2023-12-07 21:52:48 +0000

    Taskwarrior: better git renormalization
    Use an on-exit hook to renormalize Taskwarrior config files in Git when
    the context changes.

---
+++ home/.config/task/hooks/on-exit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+for arg; do
+	if [[ "$arg" = command:* ]]; then
+		if [[ "$arg" != command:context ]]; then
+			# Not running a context command, so we don't care.
+			exit 0
+		fi
+
+		exec git -C ~/.homesick/repos/castle add --renormalize home/.config/task/taskrc
+	fi
+done
+
+# If we got here, we've not found a command: argument at all, so something very
+# strange is going on!
+echo 'Failed to find a `command:` argument for on-exit hook!'
+exit 1
