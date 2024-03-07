4efbecccadaae19f2fca39cc17325897e6e20829
Adam Dinwoodie <adam@dinwoodie.org>
2024-03-07 09:00:59 +0000

    toil: move around into home directory and rebuild automatically

--- home/.local/src/toil/install.sh
+++ home/.local/src/toil/install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 #
 # Install the one (1) binary that needs compiling.
-#
-# TODO: Set things up to automatically check whether the source code is newer
-# than the executable.
-
 set -euo pipefail
 
 # Change to the directory that contains this script, just in case the script is
@@ -13,4 +9,4 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 mkdir -p ~/.local/bin
 
-gcc -Wall -O3 -o ~/.local/bin/toil src/toil.c
+gcc -Wall -O3 -o ~/.local/bin/toil toil.c
